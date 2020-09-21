8f69a7f8c9e87ed71d81a4acb8de355ca1c71c03
Kirill Rasuli <rasulikv@gmail.com>
2020-09-21 20:45:01 +0300

    Format phone numbers

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,10 @@ from .models import Flat, Complaint
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address']
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'new_building', 'construction_year']
+    list_display = [
+        'address', 'price', 'new_building',
+        'construction_year','owners_phonenumber', 'owner_pure_phone'
+    ]
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['liked_by']
