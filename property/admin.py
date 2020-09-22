26a9279d32692b746afdc8a9899888030c030a80
Kirill Rasuli <rasulikv@gmail.com>
2020-09-22 20:27:39 +0300

    Fix typo

--- property/admin.py
+++ property/admin.py
@@ -9,7 +9,7 @@ class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at']
     list_display = [
         'address', 'price', 'new_building',
-        'construction_year','owners_phonenumber', 'owner_pure_phone'
+        'construction_year', 'owners_phonenumber', 'owner_pure_phone'
     ]
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
