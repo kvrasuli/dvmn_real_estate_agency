ab8db1141549e955c8ab1e141251193c64569a1c
Kirill Rasuli <rasulikv@gmail.com>
2020-09-22 20:27:01 +0300

    Rename Owner field

--- property/models.py
+++ property/models.py
@@ -51,7 +51,7 @@ class Complaint(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField("ФИО владельца", max_length=200, db_index=True)
+    full_name = models.CharField("ФИО владельца", max_length=200, db_index=True)
     owners_phonenumber = models.CharField(
         "Номер владельца", max_length=20, db_index=True
     )
@@ -64,4 +64,4 @@ class Owner(models.Model):
     )
 
     def __str__(self):
-        return f'{self.owner}'
+        return f'{self.full_name}'
