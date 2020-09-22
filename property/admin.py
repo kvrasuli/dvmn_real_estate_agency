f51b9230dea7595798668e7bb92e1f96230f547b
Kirill Rasuli <rasulikv@gmail.com>
2020-09-22 21:28:45 +0300

    Remove redundant fields in Flat model and correct admin site

--- property/admin.py
+++ property/admin.py
@@ -8,12 +8,16 @@ class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address']
     readonly_fields = ['created_at']
     list_display = [
-        'address', 'price', 'new_building',
-        'construction_year', 'owners_phonenumber', 'owner_pure_phone'
+        'address', 'price', 'new_building', 'construction_year',
+        'get_phone_number'
     ]
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['liked_by']
+
+    def get_phone_number(self, obj):
+        return '\n'.join([str(owner.owner_pure_phone) for owner in obj.owners.all()])
+
 
 
 @admin.register(Complaint)
